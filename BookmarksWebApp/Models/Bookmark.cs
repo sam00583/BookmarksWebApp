6238074d08e43ff423515703ca6e4cac7db470ae
Samuel M <73625335+sam00583@users.noreply.github.com>
2021-01-28 09:58:51 -0500

    Add project files.

---
+++ BookmarksWebApp/Models/Bookmark.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookmarksWebApp.Models
+{
+    public class Bookmark
+    {
+        [Key] // Précise que ces une Key (ici, ces logique comme ces déjà un Id)
+        public int Id { get; set; }
+
+        [Required] // Précise que la donnée est require
+        public string Title { get; set; }
+
+        [Required] // Précise que la donnée est require
+        [Url] // Précise que la data est un URL
+        public string Url { get; set; }
+    }
+}
